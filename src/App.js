b18fcfc59cdbd0c26e4f1202250baef5fabacf95
Gwenn LINSKI <gwenn.linski@gmail.com>
2021-08-25 15:28:03 -0700

    exo 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Expenses from "./components/Expenses";
-import expenses from "./components/ExpensesData";
+import Expenses from "./components/Expenses/Expenses";
+import expenses from "./components/Expenses/ExpensesData";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+    const addExpenseHandler = (expense) => {
+        console.log("in App.js");
+        console.log(expense)
+    }
 
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses expenses={expenses}/>
         </div>
     );
