bc3125c35f0b1d065f6325a826de44801453568d
Gwenn Linski <gwenn.linski@gmail.com>
2021-08-27 01:39:40 -0700

    ex4

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
 import "./NewExpense.css"
 import ExpenseForm from "./ExpenseForm";
+import {useState} from "react";
 
 const NewExpense = (props) => {
+
+    const [expanded, setExpanded] = useState(false);
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -11,9 +14,23 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData);
     }
 
+    const newExpenseClickHandler = () => {
+      setExpanded(!expanded)
+    }
+
+    if (!expanded) {
+        return (
+            <div className="new-expense">
+                <button type="button" onClick={newExpenseClickHandler}>
+                    Add New Expense
+                </button>
+            </div>
+        )
+    }
+
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={newExpenseClickHandler}/>
         </div>
     )
 }
